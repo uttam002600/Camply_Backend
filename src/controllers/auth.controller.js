effc9b36b4c4a3688d0e75be93db0150a4f51b62
Uttam <uttam002600@gmail.com>
2025-05-13 05:28:45 +0530

    fix 2

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -41,8 +41,8 @@ const googleLogin = asyncHandler(async (req, res) => {
   // Set cookies
   const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    secure: true,
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   };
 
